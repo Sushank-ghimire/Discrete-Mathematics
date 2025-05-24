b118101b3d255fb2e76ecea4f7742da476543f05
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-05-24 19:40:57 +0545

    Add C implementation for floor function

---
+++ Lattices & Boolean Algebra/Floor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float val = 44.99;
+	printf("Floating value : %f", val);
+	
+	// Floating value after using ceil 
+	printf("\nFloating value after using floor : %f", floor(val));
+	
+	return 0;
+}
