e4113ddb3b78715929111813d14bcd28ff1a84f3
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-05-24 19:36:31 +0545

    Remove binary executable files for OR, AND, and NOT operations; add C implementation for ceil function

---
+++ Lattices & Boolean Algebra/Ceil.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float val = 44.44;
+	printf("Floating value : %f", val);
+	
+	// Floating value after using ceil 
+	printf("\nFloating value after using ceil : %f", ceil(val));
+	
+	return 0;
+}
